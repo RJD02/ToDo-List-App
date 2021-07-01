83c471a8b0d05475275064ccf4ca1c47d84d959b
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-01 13:38:04 +0530

    Fix double listen problem

--- app.js
+++ app.js
@@ -135,11 +135,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+let port = process.env.PORT || 3000;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
+// app.listen(port);
 
 app.listen(port, function() {
   console.log("Server started on port 3000");
